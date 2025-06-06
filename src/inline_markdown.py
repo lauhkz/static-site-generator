f18926e5259abe52bc16ea856dbfa9c9628df7df
lz <lautaro_RL@outlook.com>
2025-06-06 20:07:14 -0300

    feat: split nodes delimiter

---
+++ src/inline_markdown.py
@@ -0,0 +1,26 @@
+from textnode import TextType, TextNode
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    splitted_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            splitted_nodes.append(node)
+            continue
+
+        node_text = node.text
+        splitted_text = node_text.split(delimiter)
+
+        if len(splitted_text) % 2 == 0:
+            raise ValueError("unmatching delimiter")
+
+        temp_list = []
+        for i in range(len(splitted_text)):
+            if splitted_text[i] == "":
+                continue
+            if i % 2 == 0:
+                temp_list.append(TextNode(splitted_text[i], TextType.TEXT))
+            else:
+                temp_list.append(TextNode(splitted_text[i], text_type))
+            
+        splitted_nodes.extend(temp_list)
+    return splitted_nodes
